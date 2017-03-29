c010befffbd85e8a0508581659642d030af2dafa
庄金鑫 <zhuangjinxin@hotmail.com>
2017-03-29 14:58:19 +0800

    增加RabbitMQ消息队列示例

---
+++ spring-boot-sample-amqp/src/main/java/sample/amqp/Sender.java
@@ -0,0 +1,17 @@
+package sample.amqp;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+
+public class Sender {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Scheduled(fixedDelay = 1000L)
+    public void send() {
+        this.rabbitTemplate.convertAndSend("foo", "hello");
+    }
+}
